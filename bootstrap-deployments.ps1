8e3024b7848e180f0e34899bc2c96703193e3644
Tom Howcroft <THowcroft@DXC.com>
2025-08-21 13:41:14 +0100

    Test immediate update of raw file

--- bootstrap-deployments.ps1
+++ bootstrap-deployments.ps1
@@ -1,1 +1,2 @@
 Write-Host "Hi Oscar!`nDad wrote this to be accessable from anywhere so I can setup new servers without having to upload loads of tools first!"
+Write-Host "Does it update straight away?"
