b3c5705c2eb6771d19c8972f14f8683370d01878
Tom Howcroft <THowcroft@DXC.com>
2025-08-21 14:04:29 +0100

    Install AWSPowerShell module

--- bootstrap-deployments.ps1
+++ bootstrap-deployments.ps1
@@ -1,2 +1,11 @@
-Write-Host "Hi Oscar!`nDad wrote this to be accessable from anywhere so I can setup new servers without having to upload loads of tools first!"
-Write-Host "Does it update straight away?"
+#Requires -runasadministrator
+
+Write-Host "Hi! Welcome to the Deployments Bootstap script!"
+
+if (! (Get-Module -Name "AWSPowerShell.NetCore" -ListAvailable)) {
+    Write-Host "First let's make sure the AWSPowerShell.NetCore module is installed. This will give me access to the tools I'll need to retrieve the rest of the files from S3."
+    Install-Module -Name "AWSPowerShell.NetCore" -Force -ErrorAction Stop
+}
+else {
+    Write-Host "Ah, I see you're already cultured and have these tools installed."
+}
